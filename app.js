927a0d8dae835af025845007e9dd0f81a4e341f6
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-22 15:47:28 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var names = ["Muhammad", "Anus", "Ahmed"];
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("i am ".concat(name_1));
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let names: string[] = ["Muhammad", "Anus", "Ahmed"];
+for (let name of names) {
+    console.log(`i am ${name}`);
+}
+
